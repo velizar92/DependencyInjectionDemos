411bde3d477f8c64a5169b46a09f30f9ddf03788
Velizar Gerasimov <Velizar.Gerasimov@komaxgroup.com>
2024-05-31 17:02:28 +0300

    Set comments in ClientNotifier classes for Tight Coupling and Loosely Coupling examples

--- 4. ClientNotifier/1. Tight Coupling/ClientNotifier.cs
+++ 4. ClientNotifier/1. Tight Coupling/ClientNotifier.cs
@@ -2,6 +2,12 @@
 
 namespace _4._ClientNotifier._1._Tight_Coupling
 {
+    /*Here our class ClientNotifier is directly coupled to the 3 service classes. This is bad!
+    *It is not easily testable with unit tests! The class will be always changed when
+    *we have to add additional communication service. This is not a good practice and violates
+    *the Open/Close principle from SOLID. We can introduce a bug as well (very possible). 
+    */
+  
     public class ClientNotifier
     {
         private readonly EmailService _emailService;
--- 4. ClientNotifier/2. Loose Coupling/ClientNotifier.cs
+++ 4. ClientNotifier/2. Loose Coupling/ClientNotifier.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 
 namespace _4._ClientNotifier._2._Loose_Coupling
 {
+    /*Here our class ClientNotifier is totally decoupled from concrete implementations.
+    *It relays on interface and can register any service that cover the INotificationChannel interface.
+    *The solution is very flexible and is in sync with Open/Close principle from SOLID. If now
+    *we add a new notification service this class will be not touched at all.
+    */
     public class ClientNotifier
     {
         private readonly IList<INotificationChannel> _notificationChannels;
